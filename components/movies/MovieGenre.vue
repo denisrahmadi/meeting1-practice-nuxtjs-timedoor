13925229dfae05e0319b370bde8e3b21ac74a519
unknown <you@example.com>
2023-02-03 22:16:09 +0700

    first commit latihan intro nuxt (belom lengkap2 bgt functionnya)

---
+++ components/movies/MovieGenre.vue
@@ -0,0 +1,121 @@
+<template>
+  <div>
+    <div class="featured__controls">
+      <ul>
+        <li @click="filterCategoryMovie('')">All</li>
+        <li @click="filterCategoryMovie('Actions')">Actions</li>
+        <li @click="filterCategoryMovie('Science Fiction')">Science Fiction</li>
+        <li @click="filterCategoryMovie('Horror')">Horror</li>
+        <li @click="filterCategoryMovie('Thriller')">Thriller</li>
+        <li @click="filterCategoryMovie('Comedy')">Comedy</li>
+        <li @click="filterCategoryMovie('Romance')">Romance</li>
+        <li @click="filterCategoryMovie('Adventure')">Adventure</li>
+        <li @click="filterCategoryMovie('Fantasy')">Fantasy</li>
+      </ul>
+      {{ filterCategory }}
+    </div>
+  </div>
+</template>
+<script>
+import { X509Certificate } from 'crypto';
+
+export default {
+  data() {
+    return {
+      movieData: [
+        {
+          name: "Avatar: The Way of Water",
+          link: "https://i.ibb.co/PjB95Lr/movie1.jpg",
+          genre: "Science Fiction",
+          year: 2022,
+          review: "Avatar: The Way of Water",
+        },
+        {
+          name: "Insidious 1",
+          link: "https://i.ibb.co/vh30y4P/movie2.jpg",
+          genre: "Horror",
+          year: 2010,
+          review: "Insidious 1",
+        },
+        {
+          name: "Jumanji: The Next Level",
+          link: "https://i.ibb.co/SKjMVJK/movie5.jpg",
+          genre: "Adventure",
+          year: 2019,
+          review: "Jumanji: The Next Level",
+        },
+        {
+          name: "Avengers: Infinity War",
+          link: "https://i.ibb.co/Tg5rkBj/movie6.jpg",
+          genre: "Action",
+          year: 2018,
+          review: "Avengers: Infinity War",
+        },
+        {
+          name: "Avengers: Infinity War",
+          link: "https://i.ibb.co/Tg5rkBj/movie6.jpg",
+          genre: "Action",
+          year: 2018,
+          review: "Avengers: Infinity War",
+        },
+        {
+          name: "Avengers: Infinity War",
+          link: "https://i.ibb.co/Tg5rkBj/movie6.jpg",
+          genre: "Action",
+          year: 2018,
+          review: "Avengers: Infinity War",
+        },
+      ],
+      filterCategory: ''
+    };
+  },
+  methods: {
+    filterCategoryMovie(category) {
+      this.filterCategory = category
+      this.movieData.map((e) => {
+        e.filter((i) => {
+          if (i.genre == category) {
+            return 
+          }
+        })
+      })
+
+    },
+  },
+};
+</script>
+<style scoped>
+.featured__controls {
+  text-align: center;
+  margin-bottom: 50px;
+}
+
+.featured__controls ul li {
+  list-style: none;
+  font-size: 18px;
+  color: #1c1c1c;
+  display: inline-block;
+  margin-right: 25px;
+  position: relative;
+  cursor: pointer;
+}
+
+.featured__controls ul li.active:after {
+  opacity: 1;
+}
+
+.featured__controls ul li:after {
+  position: absolute;
+  left: 0;
+  bottom: -2px;
+  width: 100%;
+  height: 2px;
+  background: #7fad39;
+  content: "";
+  opacity: 0;
+}
+
+.featured__controls ul li:last-child {
+  margin-right: 0;
+}
+</style>
---
+++ components/movies/MovieItem.vue
@@ -0,0 +1,114 @@
+<template>
+  <!-- 
+    class="col-lg-3 col-md-4 col-sm-6" 
+
+style="display:flex; justify-content: center; flex-wrap: wrap;"
+  -->
+  <div class="berjejer">
+    <div v-for="(movie, index) in movieData" :key="index">
+      <div class="featured__item"  >
+          <img
+          class="featured__item__pic"
+          :src="movie.link"
+          />
+          <nuxt-link tag="h5" class="featured__item__text" :to="{name: 'detail', params: {detail: movie}}">
+            {{ movie.name }}
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </template>
+<script> 
+export default {
+  data() {
+    return {
+      movieData: [
+        {
+          name: "Avatar: The Way of Water",
+          link: "https://i.ibb.co/PjB95Lr/movie1.jpg",
+          genre: "Science Fiction",
+          year: 2022,
+          review: "Avatar: The Way of Water",
+        },
+        {
+          name: "Insidious 1",
+          link: "https://i.ibb.co/vh30y4P/movie2.jpg",
+          genre: "Horror",
+          year: 2010,
+          review: "Insidious 1",
+        },
+        {
+          name: "Jumanji: The Next Level",
+          link: "https://i.ibb.co/SKjMVJK/movie5.jpg",
+          genre: "Adventure",
+          year: 2019,
+          review: "Jumanji: The Next Level",
+        },
+        {
+          name: "Avengers: Infinity War",
+          link: "https://i.ibb.co/Tg5rkBj/movie6.jpg",
+          genre: "Action",
+          year: 2018,
+          review: "Avengers: Infinity War",
+        },
+        {
+          name: "Avengers: Infinity War",
+          link: "https://i.ibb.co/Tg5rkBj/movie6.jpg",
+          genre: "Action",
+          year: 2018,
+          review: "Avengers: Infinity War",
+        },
+        {
+          name: "Avengers: Infinity War",
+          link: "https://i.ibb.co/Tg5rkBj/movie6.jpg",
+          genre: "Action",
+          year: 2018,
+          review: "Avengers: Infinity War",
+        },
+      ],
+    };
+  }
+}
+</script>
+<style scoped>
+
+.berjejer{
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.featured__item {
+  margin-bottom: 50px;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-top: 20px;
+  display: grid;
+
+}
+
+.featured__item__pic {
+  height: 330px;
+  width: 270px;
+  position: relative;
+  overflow: hidden;
+  background-position: center center;
+}
+
+.featured__item__text {
+  text-align: center;
+  padding-top: 15px;
+}
+
+.featured__item__text h6 {
+  margin-bottom: 10px;
+}
+
+.featured__item__text h6 a {
+  color: #252525;
+}
+
+.featured__item__text h5 {
+  color: #252525;
+  font-weight: 700;
+}
+</style>
